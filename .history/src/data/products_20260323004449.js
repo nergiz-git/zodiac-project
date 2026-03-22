d22a86db466cbb30a85e80202293fc1cf6776025
Nərgiz <nergizmehdiyeva051@gmail.com>
2026-03-23 03:30:30 +0400

    first commit

---
+++ .history/src/data/products_20260323004449.js
@@ -0,0 +1,163 @@
+export const products = [
+  {
+   id: 1,
+   name: "Orange Calcite",
+   price: 47.00,
+   img: "/src/assets/images/mineral-6-600x600.png",
+   description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+   categories: ["Astrology", "Crystals", "Mystical"],
+   tags: ["aroma", "divination", "numbers", "prediction"]
+   },
+  { 
+    id: 2,
+    name: "Green Sardonyx",
+    price: 54.00,
+    img: "/src/assets/images/mineral-2-600x600.png",
+    description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+   categories: ["Astrology", "Crystals", "Mystical"],
+   tags: ["aroma", "divination", "numbers", "prediction"]
+   },
+  {
+   id: 3,
+   name: "Red Jasper",
+   price: 35.00,
+   img: "/src/assets/images/mineral-4-600x600.png",
+   description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+   categories: ["Astrology", "Crystals", "Mystical"],
+   tags: ["aroma", "divination", "numbers", "prediction"]
+   },
+  {
+   id: 4,
+   name: "Yellow Jasper",
+   price: 65.00,
+   img: "/src/assets/images/mineral-1-600x600.png",
+   description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+   categories: ["Astrology", "Crystals", "Mystical"],
+   tags: ["aroma", "divination", "numbers", "prediction"]
+  },
+  {
+   id: 5,
+   name: "Cabochon Red Agate", 
+   price: 65.00, 
+   img: "/src/assets/images/mineral-5-600x600.png",
+   description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+   categories: ["Astrology", "Crystals", "Mystical"],
+   tags: ["aroma", "divination", "numbers", "prediction"]
+  },
+  { 
+    id: 6, 
+    name: "Akik Stone", 
+    price: 50.00, 
+    img: "/src/assets/images/mineral-3-600x600.png",
+    description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+    categories: ["Astrology", "Crystals", "Mystical"],
+    tags: ["aroma", "divination", "numbers", "prediction"]
+  },
+  { 
+    id: 7, 
+    name: "Palmistry Book", 
+    price: 340.00, 
+    img: "/src/assets/images/palmistry-book-300x300.png",
+    description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+    categories: ["Astrology", "Crystals", "Mystical"],
+    tags: ["aroma", "divination", "numbers", "prediction"]
+  },
+  { id: 8, 
+    name: "Palmistry Book White", 
+    price: 239.00, 
+    img: "/src/assets/images/palmistry-book-w-300x300.png",
+    description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+    categories: ["Astrology", "Crystals", "Mystical"],
+    tags: ["aroma", "divination", "numbers", "prediction"]
+  },
+  {
+     id: 9, 
+     name: "Astrology Book", 
+     price: 267.00, 
+     img: "/src/assets/images/astrology-book-300x300 (1).png",
+     description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+     categories: ["Astrology", "Crystals", "Mystical"],
+     tags: ["aroma", "divination", "numbers", "prediction"]
+    },
+  {
+     id: 10, 
+     name: "Astrology Book White", 
+     price: 240.00, 
+     img: "/src/assets/images/astrology-book-w-300x300.png",
+     description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+     categories: ["Astrology", "Crystals", "Mystical"],
+     tags: ["aroma", "divination", "numbers", "prediction"]
+    },
+  {
+     id: 11, 
+     name: "Tarot Book", 
+     price: 242.00, 
+     img: "/src/assets/images/tarot-book-2-300x300.png",
+     description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+     categories: ["Astrology", "Crystals", "Mystical"],
+     tags: ["aroma", "divination", "numbers", "prediction"]
+    },
+  {
+    id: 12, 
+    name: "Tarot Book White", 
+    price: 242.00, 
+    img: "/src/assets/images/tarot-book-300x300.png",
+    description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+    categories: ["Astrology", "Crystals", "Mystical"],
+    tags: ["aroma", "divination", "numbers", "prediction"] 
+  },
+  { 
+    id: 13, 
+    name: "Mysta Tarot Cards Dark", 
+    price: 374.00, 
+    img: "/src/assets/images/tarot-cards-300x300.png",
+    description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+    categories: ["Astrology", "Crystals", "Mystical"],
+    tags: ["aroma", "divination", "numbers", "prediction"]
+  },
+  { 
+    id: 14, 
+    name: "Mysta Tarot Cards Light", 
+    price: 267.00, 
+    img: "/src/assets/images/tarot-cards-2-300x300.png",
+    description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+    categories: ["Astrology", "Crystals", "Mystical"],
+    tags: ["aroma", "divination", "numbers", "prediction"]
+  },
+  {
+     id: 15, 
+     name: "Mysta Tarot Cards Gold", 
+     price: 450.00, 
+     img: "/src/assets/images/tarot-cards-3-300x300.png",
+     description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+     categories: ["Astrology", "Crystals", "Mystical"],
+     tags: ["aroma", "divination", "numbers", "prediction"]
+    },
+  {
+     id: 16,
+    name: "Scented Candle Orange", 
+    price: 65.00, 
+    img: "/src/assets/images/candle-79-300x300.png",
+    description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+    categories: ["Astrology", "Crystals", "Mystical"],
+    tags: ["aroma", "divination", "numbers", "prediction"] 
+  },
+  {
+    id: 17, name: "Scented Candle Orange", 
+    price: 65.00, 
+    img: "/src/assets/images/candle-06-300x300.png",
+    description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+    categories: ["Astrology", "Crystals", "Mystical"],
+    tags: ["aroma", "divination", "numbers", "prediction"]
+  },
+  {
+     id: 18, 
+     name: "Scented Candle Jasmine", 
+     price: 24.00, 
+     img: "/src/assets/images/08-candle-300x300.png",
+     description: "Praesent ullamcorper erat eu mattis commodo. Sed elementum lacinia massa eu hendrerit.",
+     categories: ["Astrology", "Crystals", "Mystical"],
+     tags: ["aroma", "divination", "numbers", "prediction"] 
+    },
+];
+export default products;
